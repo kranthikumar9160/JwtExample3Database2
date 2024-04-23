2c097fd85f463260b0cac50ea8adc343be6b5e2f
kranthikumar9160 <44632777+kranthikumar9160@users.noreply.github.com>
2024-04-23 17:28:26 +0530

    Added print stack trace

--- src/main/java/com/jwt/example/config/SecurityConfig.java
+++ src/main/java/com/jwt/example/config/SecurityConfig.java
@@ -3,13 +3,19 @@ package com.jwt.example.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.jwt.example.security.CustomUserDetailService;
 import com.jwt.example.security.JwtAuthenticationEntryPoint;
 import com.jwt.example.security.JwtAuthenticationFilter;
 
@@ -23,23 +29,68 @@ public class SecurityConfig {
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//		http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+//				.authorizeHttpRequests(auth -> auth.requestMatchers("/home").authenticated()
+//						.requestMatchers("/auth/login").permitAll().requestMatchers("/export").authenticated().anyRequest().authenticated())
+//				.exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
+//				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+////		http.csrf(csrf->csrf.disable())
+////		.cors(cors->cors.disable())
+////		.authorizeHttpRequests(auth->auth.requestMatchers("/addUser").permitAll().anyRequest().authenticated())
+////		.exceptionHandling(ex->ex.authenticationEntryPoint(authenticationEntryPoint))
+////		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//		return http.build();
+//	}
+	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-		http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/home").authenticated()
-						.requestMatchers("/auth/login").permitAll().anyRequest().authenticated())
-				.exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint))
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-//		http.csrf(csrf->csrf.disable())
-//		.cors(cors->cors.disable())
-//		.authorizeHttpRequests(auth->auth.requestMatchers("/addUser").permitAll().anyRequest().authenticated())
-//		.exceptionHandling(ex->ex.authenticationEntryPoint(authenticationEntryPoint))
-//		.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-		return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+//                .requestMatchers("/products/new","/products/authenticate").permitAll()
+        		.requestMatchers("/auth/login").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/home/**")
+                .authenticated().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+	
+	@Bean
+    //authentication
+    public UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withUsername("Basant")
+//                .password(encoder.encode("Pwd1"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails user = User.withUsername("John")
+//                .password(encoder.encode("Pwd2"))
+//                .roles("USER","ADMIN","HR")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+        return new CustomUserDetailService();
+    }
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
 }
