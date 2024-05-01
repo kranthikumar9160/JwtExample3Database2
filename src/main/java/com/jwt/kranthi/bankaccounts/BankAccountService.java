d772d1f83efe4e9a9208693cdbb5d7945aae00b5
kranthikumar9160 <44632777+kranthikumar9160@users.noreply.github.com>
2024-05-01 12:01:29 +0530

    Changed package name

--- src/main/java/com/jwt/kranthi/bankaccounts/BankAccountService.java
+++ src/main/java/com/jwt/kranthi/bankaccounts/BankAccountService.java
@@ -1,7 +1,11 @@
-package com.jwt.example.bankaccounts;
+package com.jwt.kranthi.bankaccounts;
 
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -9,12 +13,16 @@ import java.util.Vector;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.groups.GroupsUtils;
 import com.groups.dao.GroupsBaseAccess;
 import com.groups.ems.accounts.action.PaginationBean;
 import com.groups.ems.accounts.action.ViewPageBean;
-import com.jwt.example.utils.KranthiBaseAccess;
-import com.jwt.example.utils.KranthiConstants;
-import com.jwt.example.utils.KranthiUtils;
+import com.groups.ems.bankaccounts.action.GroupsBean;
+import com.groups.ems.bankaccounts.action.PaginationHelper;
+import com.groups.ems.bankaccounts.form.ViewBankAccountForm;
+import com.jwt.kranthi.utils.KranthiBaseAccess;
+import com.jwt.kranthi.utils.KranthiConstants;
+import com.jwt.kranthi.utils.KranthiUtils;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -70,7 +78,9 @@ public class BankAccountService {
 
 //		GroupsBaseAccess.executeQuery(query, conArg, vt, dataType);
 		
-		bankAccountRepository.executeQuery(query);
+		ResultSet rs = bankAccountRepository.executeQuery(query);
+		
+		KranthiBaseAccess.executeQuery(query, vt, dataType, rs);
 		
 		return vt;
 	}
@@ -123,9 +133,71 @@ public class BankAccountService {
 		return where.toString();
 	}
 	
-	public Vector getViewRows(HttpSession session, String where,
-			PaginationBean pbean, Connection conArg) throws SQLException
+	
+	protected void setViewRows(ViewBankAccountForm formArg,	boolean searchArg) throws SQLException
 	{
+		Vector vtRows = new Vector();
+		String where = getQueryString(formArg);
+
+		PagenationBean pbean = formArg.getPaginationBean();
+
+		vtRows = getViewRows(where, pbean);
+
+		String total = pbean.getTotal(); // (String)
+											// session.getAttribute(GroupsConstants.TOTAL_ROWS_COUNT);
+
+		int ttl = KranthiUtils.getInt(total);
+		if (ttl > 0)
+		{
+			if ("update".equalsIgnoreCase(pbean.getType()))
+			{
+				PaginationHelper ph = new PaginationHelper(session);
+				ph.updateRecordSet(pbean, connectionArg);
+			}
+			
+			if(pbean.isRowsUpdated())
+				vtRows = getViewRows(session, where, pbean, connectionArg); 
+		}
+
+		if (vtRows.size() > 0)
+		{
+			if ("update".equalsIgnoreCase(pbean.getType()))
+			{
+				PaginationHelper ph = new PaginationHelper(session);
+				ph.updateRecordSet(pbean, connectionArg);
+			}
+
+			if (searchArg)
+			{
+				GroupsBean bean = (GroupsBean) vtRows.get(0);
+
+				String prop1 = bean.getStrId();
+
+				formArg.setSelectedId(prop1);
+			}
+			else
+			{
+				String prop1 = formArg.getSelectedId();
+				prop1 = getSelectedIdVal(vtRows, prop1, -1);
+				formArg.setSelectedId(prop1);
+			}
+		}
+		else
+		{
+			formArg.setSelectedId("");
+			pbean.reset();
+		}
+
+		formArg.setPaginationBean(pbean);
+
+		session.setAttribute("PaginationBean", pbean);
+		session.setAttribute("vtList", vtRows);
+	}
+	
+	public Vector getViewRows(String where,
+			PagenationBean pbean) throws SQLException
+	{
+		
 		HashMap svb = new HashMap();
 		int offset;
 		int end;
@@ -133,7 +205,7 @@ public class BankAccountService {
 		end = pbean.getEndInt();
 
 //		svb.put(GroupsBaseAccess.TABLE_NAME, finTable);
-		svb.put(KranthiBaseAccess.TABLE_NAME, getTable(session));
+		svb.put(KranthiBaseAccess.TABLE_NAME, getTable());
 		svb.put("where", where);
 		svb.put("account_seq", "");
 		svb.put("financial_year_seq", "");
@@ -147,8 +219,8 @@ public class BankAccountService {
 		svb.put("status_cd", "");
 		svb.put("status_name", "");
 
-		ViewPageBean viewPageBean = GroupsBaseAccess.getViewRows(svb,
-				String.valueOf(offset), String.valueOf(end), conArg);
+		ViewPageBean viewPageBean = KranthiBaseAccess.getViewRows(svb,
+				String.valueOf(offset), String.valueOf(end));
 
 		pbean.setTotal(viewPageBean.getTotalRowCount() + "");
 		session.setAttribute(KranthiConstants.TOTAL_ROWS_COUNT,//
@@ -172,7 +244,106 @@ public class BankAccountService {
 
 	}
 	
-	public static Vector getRows(HashMap hmData)
+	public static ViewPageBean getViewRows(HashMap columns, String offset,
+			String end, Connection connectionArg) throws SQLException
+	{
+		String query = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		Vector vt = new Vector();
+		int noOfRecords = 0;
+		ViewPageBean viewPageBean = new ViewPageBean(noOfRecords, vt);
+		if (columns == null) return null;
+
+		String where = "";
+		String distinct = "";
+		String orderBy = "";
+		if (columns.containsKey("distinct"))
+		{
+			columns.remove("distinct");
+			distinct = " distinct ";
+		}
+
+		if (columns.containsKey("orderby"))
+		{
+			orderBy = (String) columns.remove("orderby");
+		}
+		if (columns.containsKey("where"))
+		{
+			where = (String) columns.remove("where");
+			where = " where  " + where;
+		}
+
+		String table = (String) columns.remove(TABLE_NAME);
+
+		if (columns.size() == 0)
+		{
+			return null;
+		}
+
+		Set s = columns.keySet();
+
+		String cols = s.toString();
+
+		//vt.addAll(s);
+
+		cols = removeFirst(cols);
+
+		query = "select " + distinct + " " + cols + " from " + table + where
+				+ " " + orderBy;
+
+		query = " select SQL_CALC_FOUND_ROWS * from (" + query + ")a limit "
+				+ offset + ", " + end;
+
+		Object obj[] = s.toArray();
+
+		try
+		{
+			System.out.println(" " + query);
+			stmt = connectionArg.createStatement();
+			rs = stmt.executeQuery(query);
+			while (rs.next())
+			{
+				HashMap mp = new HashMap();
+				for (int i = 0; i < obj.length; i++)
+				{
+					String col = (String) obj[i];
+					
+					String dataType = GroupsUtils.nullToEmpty(columns
+							.get(col));
+					
+					setResultSetVal(col,dataType,mp,rs);
+				}
+				vt.add(mp);
+			}
+			rs.close();
+
+			rs = stmt.executeQuery("SELECT FOUND_ROWS() total ");
+			if (rs.next()) noOfRecords = rs.getInt("total");
+
+			viewPageBean.setTotalRowCount(noOfRecords);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			throw e;
+		}
+		finally
+		{
+			try
+			{
+				if (stmt != null) stmt.close();
+
+			}
+			catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return viewPageBean;
+	}
+	
+	public Vector getRows(HashMap hmData)
 			throws SQLException
 	{
 		HashMap dataType = new HashMap();
@@ -197,7 +368,7 @@ public class BankAccountService {
 	
 	
 	
-	public String getTable(HttpSession session) 
+	public String getTable() 
 	{
 //		UserSchoolLimits usl = new UserSchoolLimits(session);
 //		String query = usl.getUserLimitQuery();
@@ -221,4 +392,5 @@ public class BankAccountService {
 		return strData;
 	}
 	
+
 }
