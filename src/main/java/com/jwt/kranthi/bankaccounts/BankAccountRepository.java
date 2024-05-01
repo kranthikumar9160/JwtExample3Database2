2bc270fb4ac27f28409d45fee2ba07b9a58e1195
kranthikumar9160 <44632777+kranthikumar9160@users.noreply.github.com>
2024-05-01 12:00:57 +0530

    Changed package name

--- src/main/java/com/jwt/kranthi/bankaccounts/BankAccountRepository.java
+++ src/main/java/com/jwt/kranthi/bankaccounts/BankAccountRepository.java
@@ -1,14 +1,16 @@
-package com.jwt.example.bankaccounts;
+package com.jwt.kranthi.bankaccounts;
+
+import java.sql.ResultSet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.jwt.example.models.User;
+import com.jwt.kranthi.models.User;
 
 public interface BankAccountRepository extends JpaRepository<User, String> {
 
 	public User findByEmail(String email);
 	
 	@Query(value=":query", nativeQuery = true)
-	public void executeQuery(String query);
+	public ResultSet executeQuery(String query);
 }
